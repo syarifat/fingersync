5489aa4bff7cc6dbc72fa7b8c9b2dcbd381df46c
syarifat <syarifahsanit@gmail.com>
2026-02-02 14:43:48 +0700

    navbar absensi view

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -135,6 +135,16 @@
                 <span :class="sidebarOpen ? 'opacity-100 ml-4' : 'opacity-0 w-0'" class="font-medium transition-all duration-300 overflow-hidden whitespace-nowrap">Jadwal Pelajaran</span>
             </a>
 
+            <a href="{{ route('admin.presensi.index') }}" 
+                class="flex items-center p-3 rounded-xl transition-all duration-200 group {{ request()->routeIs('admin.presensi.*') ? 'bg-orange-700 shadow-inner' : 'hover:bg-orange-500' }}">
+                <svg class="w-6 h-6 shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+                </svg>
+                <span :class="sidebarOpen ? 'opacity-100 ml-4' : 'opacity-0 w-0'" class="font-medium transition-all duration-300 overflow-hidden whitespace-nowrap">
+                    Data Absensi
+                </span>
+            </a>
+
         </div>
         @endif
 
